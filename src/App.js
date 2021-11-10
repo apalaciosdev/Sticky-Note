4cc592221548e8a19d0ef091908c552b87b4cad9
apalacios <apalacios.dev@gmail.com>
2021-11-10 14:40:13 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Stage, Layer } from "react-konva";
+import { StickyNote } from "./StickyNote";
 
-function App() {
+
+const App = () => {
+  const [text, setText] = useState("Click to resize. Double click to edit.");
+  const [width, setWidth] = useState(200);
+  const [height, setHeight] = useState(200);
+  const [selected, setSelected] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+    <Stage
+      width={window.innerWidth}
+      height={window.innerHeight}
+      onClick={(e) => {
+        if (e.currentTarget._id === e.target._id) {
+          setSelected(false);
+        }
+      }}
+    >
+      <Layer>
+        <StickyNote
+          x={50}
+          y={50}
+          text={text}
+          colour="#FFDAE1"
+          onTextChange={(value) => setText(value)}
+          width={width}
+          height={height}
+          selected={selected}
+          onTextResize={(newWidth, newHeight) => {
+            setWidth(newWidth);
+            setHeight(newHeight);
+          }}
+          onClick={() => {
+            setSelected(!selected);
+          }}
+          onTextClick={(newSelected) => {
+            setSelected(newSelected);
+          }}
+        />
+      </Layer>
+    </Stage>
+
+ 
+      <Layer>
+        <StickyNote
+          x={50}
+          y={50}
+          text={text}
+          colour="#FFDAE1"
+          onTextChange={(value) => setText(value)}
+          width={width}
+          height={height}
+          selected={selected}
+          onTextResize={(newWidth, newHeight) => {
+            setWidth(newWidth);
+            setHeight(newHeight);
+          }}
+          onClick={() => {
+            setSelected(!selected);
+          }}
+          onTextClick={(newSelected) => {
+            setSelected(newSelected);
+          }}
+        />
+      </Layer>
+
     </div>
+
+    
+    
   );
-}
+};
+
+
+
 
 export default App;
